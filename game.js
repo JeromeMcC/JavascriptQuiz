2202068f07bc664b04430c055438924b49751598
Jerome McClarin <Jerome.Mcclarin@gmail.com>
2022-01-05 15:05:41 -0500

    updated functionality

--- game.js
+++ game.js
@@ -25,55 +25,70 @@ var choice1 = document.querySelector("#answerA")
 var choice2 = document.querySelector("#answerB")
 var choice3 = document.querySelector("#answerC")
 var choice4 = document.querySelector("#answerD")
-
+var questionIndex;
 let questions = [
     {
 
         question: "What is 2 + 2?",
         choices: ["2", "4", "6", "8"],
-        answer: 1,
+        answer: "4",
     },
     {
         question: "What is 2 + 4?",
         choices: ["2", "4", "6", "8"],
-        answer: 2,
+        answer: "6",
     },
     {
         question: "What is 2 + 6?",
         choices: ["2", "4", "6", "8"],
-        answer: 3,
+        answer: "8",
     },
     {
         question: "What is 2 + 10?",
         choices: ["2", "4", "6", "12"],
-        answer: 3,
+        answer: "12",
     },
     {
         question: "What is 2 + 12?",
         choices: ["2", "14", "6", "8"],
-        answer: 1,
+        answer: "14",
     }
 ]
 var SCORE = 100
 var QUESTION = 5
+var seconds = 60
 //WHEN I click the start button
-//THEN a timer starts and I am presented with a question
+
+choice1.addEventListener("click", function () {
+    answerEval(choice1.innerHTML, questionIndex.answer)
+})
+choice2.addEventListener("click", function () {
+    answerEval(choice2.innerHTML, questionIndex.answer)
+})
+choice3.addEventListener("click", function () {
+    answerEval(choice3.innerHTML, questionIndex.answer)
+})
+choice4.addEventListener("click", function () {
+    answerEval(choice4.innerHTML, questionIndex.answer)
+})
+
 startQuiz = function () {
     //location.href="/game.html"
     startTimer()
     questionCounter = 0
-    score = 0
+    SCORE = 0
     availableQuestions = [...questions]
     getNewQuestion()
 
 }
 startTimer = function () {
-    let seconds = 60;
     timerInterval = setInterval(function () {
-        document.getElementById("quiz-timer").innerHTML = "00:" + seconds;
-        seconds--;
-        if (seconds < 0) {
+         seconds--;
+         document.getElementById("quiz-timer").innerHTML = "00:" + seconds;
+       
+        if (seconds <= 0) {
             clearInterval(timerInterval);
+            console.log("done")
         }
     }, 1000);
     console.log("i'm working")
@@ -82,7 +97,7 @@ startTimer = function () {
 getNewQuestion = function () {
 
     let questionPrompt = document.getElementById("question");
-    var questionIndex = questions[Math.floor(Math.random() * 5)]
+     questionIndex = questions[Math.floor(Math.random() * 5)]
     var option1 = questionIndex.choices[0]
     var option2 = questionIndex.choices[1]
     var option3 = questionIndex.choices[2]
@@ -94,35 +109,48 @@ getNewQuestion = function () {
     choice4.innerHTML = option4;
 
 
-    choice1.addEventListener("click", function () {
-        answerEval(option1, questionIndex.answer)
-    })
-    choice2.addEventListener("click", function () {
-        answerEval(option2, questionIndex.answer)
-    })
-    choice3.addEventListener("click", function () {
-        answerEval(option3, questionIndex.answer)
-    })
-    choice4.addEventListener("click", function () {
-        answerEval(option4, questionIndex.answer)
-    })
 }
-function answerEval(userchoice,answer) {
-    let seconds= 60
+function answerEval(userchoice,answer) 
+{
+    console.log(userchoice);
+    console.log(answer);
+    QUESTION-=1
+    
     if (userchoice === answer){
         //addtime and turn green
-        seconds+=15 
-        document.querySelector(".choice-container").classList.add("correct");
+        seconds+=15;
+        SCORE+=20;
+
+        
+    //score
+        console.log(seconds) 
+        //document.querySelector(".choice-container").classList.add("correct");
         console.log("correct")}
      else{ 
-        seconds-=15
-        document.querySelector(".choice-container").classList.add("incorrect");}
-//subract time and turn red
+        seconds-=15;
+        SCORE-=20;
+        
+        //score
+        //document.querySelector(".choice-container").classList.add("incorrect");
+    
+        console.log("incorrect")
     }
+    console.log(SCORE)
+//subract time and turn red
 
 
 
+        if ( QUESTION <= 0) {
+            console.log ("done");
+            //return gameover
+        } 
+ getNewQuestion()
+}
 
+//if (( QUESTION <= 0) || (seconds <= 0)) {
+    //console.log (done);
+    //return gameover
+//} 
 
 startQuiz()
 
